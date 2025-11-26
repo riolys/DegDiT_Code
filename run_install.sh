5acd0de0017722f5c95f549529033f8dd6b3972f
Yisu Liu <93517803+riolys@users.noreply.github.com>
2025-11-26 16:30:11 +0800

    Environment installation

---
+++ run_install.sh
@@ -0,0 +1,9 @@
+#install https://github.com/descriptinc/audiotools firstly, then conduct this file with "bash run_install.sh"
+pip install diffusers -i https://mirrors.cloud.tencent.com/pypi/simple
+pip install accelerate==0.34.2 -i https://mirrors.cloud.tencent.com/pypi/simple
+pip install datasets==2.21.0 -i https://mirrors.cloud.tencent.com/pypi/simple
+pip install transformers==4.44.0 -i https://mirrors.cloud.tencent.com/pypi/simple
+pip install soundfile -i https://mirrors.cloud.tencent.com/pypi/simple
+pip install tiktoken -i https://mirrors.cloud.tencent.com/pypi/simple
+pip install sentencepiece -i https://mirrors.cloud.tencent.com/pypi/simple
+pip install ipdb -i https://mirrors.cloud.tencent.com/pypi/simple
